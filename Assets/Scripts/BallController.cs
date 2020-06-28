988184da477bf23cda4baf218d4827e590340dc3
Crudelis <ann.crudelis@gmail.com>
2020-06-28 20:22:25 +0600

    Ball controller and platform feature

--- Assets/Scripts/BallController.cs
+++ Assets/Scripts/BallController.cs
@@ -4,15 +4,32 @@ using UnityEngine;
 
 public class BallController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    Rigidbody2D rb;
+    private float powerForce = 7f; // сила толчка 
+    Vector3 direction = Vector3.zero;
+    private void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Platform")
+        {
+            gameObject.GetComponent<Rigidbody2D>().AddForce((Vector3.up + direction) * powerForce, ForceMode2D.Impulse);
+        }
+
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        //Debug.Log(Input.mousePosition);
+        if (Input.GetMouseButtonDown(0))
+        {
+            direction.x = Camera.main.ScreenToWorldPoint(Input.mousePosition).x/2;
+            
+            gameObject.GetComponent<Rigidbody2D>().AddForce(direction * powerForce, ForceMode2D.Impulse);
+
+
+        }
     }
 }
---
+++ Assets/Scripts/Platform.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Platform : MonoBehaviour
+{
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            this.gameObject.GetComponent<SpriteRenderer>().color = new Color(Random.Range(0f, 1f), Random.Range(0f, 1f), Random.Range(0f, 1f));
+        }
+    }
+}
